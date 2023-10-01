58dd3454caa6743f7ab8eb0bc49cb4097ae7ab7a
ltr <apuroopgupca007@gmail.com>
2023-10-01 13:42:42 +0530

    Created slice for movies and configured store

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Login from "./Login";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import moviesReducer from "./moviesSlice";
 
 export const appStore = configureStore({
   reducer: {
     user: userReducer,
+    movies: moviesReducer,
   },
 });
