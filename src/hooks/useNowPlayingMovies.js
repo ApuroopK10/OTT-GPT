535615c7d29a0f866d586d198f3d8250674f56f0
ltr <apuroopgupca007@gmail.com>
2023-10-01 13:43:03 +0530

    Created custom hook to fetch NowPlaying movies

---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS, NOW_PLAYING } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+  const fetchNowPlaying = async () => {
+    const data = await fetch(NOW_PLAYING, API_OPTIONS);
+    const json = await data.json();
+    dispatch(addNowPlayingMovies(json.results));
+    console.log("json", json);
+  };
+
+  useEffect(() => {
+    fetchNowPlaying();
+  }, []);
+};
+export default useNowPlayingMovies;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,13 @@ export const LOGO_URL =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_ICON =
   "https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABVuTdvORo4497Xv52ZEdug-y_55v2YEYxibSY4NqYqBeDpDnzJLih35zdoLCecFznv_ngn5t6yk_cF7eCUXZTt2_6Au0fNA.png?r=805";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDliYTFhNTQzMmRhMjA5OGM5ZWZjNmRiYzE5ZDEwOCIsInN1YiI6IjY1MTkxZjkyOWQ1OTJjMDE0MjYwZmM4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d8kywHOKV4phhvou9B07aTlabKTFGYps8xf_7OI34nk",
+  },
+};
+export const NOW_PLAYING =
+  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
