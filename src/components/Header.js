4428ed660d4a2527fab6294b063bf8a06798f29f
ltr <apuroopgupca007@gmail.com>
2023-10-01 12:55:07 +0530

    Added Constants, Cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO_URL, USER_ICON } from "../utils/constants";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -34,18 +35,10 @@ const Header = () => {
   };
   return (
     <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        alt="logo"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-      />
+      <img className="w-44" alt="logo" src={LOGO_URL} />
       {userData && (
         <div className="flex items-center justify-center">
-          <img
-            className="w-8 h-8"
-            src="https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABVuTdvORo4497Xv52ZEdug-y_55v2YEYxibSY4NqYqBeDpDnzJLih35zdoLCecFznv_ngn5t6yk_cF7eCUXZTt2_6Au0fNA.png?r=805"
-            alt="usericon"
-          />
+          <img className="w-8 h-8" src={USER_ICON} alt="usericon" />
           <button className="h-8 text-white font-bold" onClick={handleSignOut}>
             Sign Out
           </button>
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const LOGO_URL =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const USER_ICON =
+  "https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABVuTdvORo4497Xv52ZEdug-y_55v2YEYxibSY4NqYqBeDpDnzJLih35zdoLCecFznv_ngn5t6yk_cF7eCUXZTt2_6Au0fNA.png?r=805";
