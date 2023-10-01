5ad0d11117004c7012ac881bba9305e8bc3a9061
ltr <akambampati@hcf.com.au>
2023-10-01 12:21:12 +0530

    Updated Header and Browse components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,40 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
         alt="logo"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
       />
+      {userData && (
+        <div className="flex items-center justify-center">
+          <img
+            className="w-8 h-8"
+            src="https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABVuTdvORo4497Xv52ZEdug-y_55v2YEYxibSY4NqYqBeDpDnzJLih35zdoLCecFznv_ngn5t6yk_cF7eCUXZTt2_6Au0fNA.png?r=805"
+            alt="usericon"
+          />
+          <button className="h-8 text-white font-bold" onClick={handleSignOut}>
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
