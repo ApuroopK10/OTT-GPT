aeb0c04c77523fad2262c0dd82704dbb9916c0b9
ltr <akambampati@hcf.com.au>
2023-10-01 12:26:21 +0530

    Added Authentication using Firebase for Sign in/Sign up/Logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,10 +6,12 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
   const [errorMsg, setErrorMsg] = useState(null);
+  const navigate = useNavigate();
   const email = useRef(null);
   const password = useRef(null);
   const fullName = useRef(null);
@@ -38,8 +40,7 @@ const Login = () => {
         )
           .then((userCredential) => {
             // Signed in
-            const user = userCredential.user;
-            console.log(user, "succeess");
+            navigate("/Browse");
           })
           .catch((error) => {
             const errorCode = error.code;
@@ -54,8 +55,7 @@ const Login = () => {
         )
           .then((userCredential) => {
             // Signed up
-            const user = userCredential.user;
-            console.log(user, "succeess");
+            navigate("/Browse");
           })
           .catch((error) => {
             const errorCode = error.code;
@@ -74,7 +74,7 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
         />
       </div>
-      <form className="w-1/3 absolute p-12 text-white bg-slate-950 bg-opacity-80 mx-auto my-36 right-0 left-0 flex flex-col rounded-md">
+      <form className="w-screen md:w-1/3 absolute p-12 text-white bg-slate-950 bg-opacity-80 mx-auto my-0 md:my-36 right-0 left-0 flex flex-col rounded-md">
         <h1 className="text-3xl font-bold py-4 m-2">
           {isSignIn ? "Sign In" : "Sign Up"}
         </h1>
