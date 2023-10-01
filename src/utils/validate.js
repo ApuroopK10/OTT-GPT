d32ef28487887e8b3b72b9a79aec1c222313577b
ltr <akambampati@hcf.com.au>
2023-10-01 12:21:47 +0530

    Added Authentication using Firebase for Sign in/Sign up/Logout

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,5 @@
 export const isValidData = (email, password, fullName) => {
+  // eslint-disable-next-line no-useless-escape
   const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
