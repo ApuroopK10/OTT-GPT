11aa4a272605f929271c59797679cbe7964ce7b9
ltr <apuroopgupca007@gmail.com>
2023-10-02 13:15:24 +0530

    Created custom hook to fetch all categories and avoided duplicity

---
+++ src/hooks/useMovieCategories.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { API_OPTIONS, FETCH_CATEGORIES } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addAllCats } from "../utils/moviesSlice";
+
+const useMovieCategories = (types) => {
+  const dispatch = useDispatch();
+  const fetchAllCats = async () => {
+    const fetchCatsArr = [];
+    types.forEach((cat) => {
+      fetchCatsArr.push(
+        fetch(FETCH_CATEGORIES.replace("{type}", cat), API_OPTIONS)
+      );
+    });
+    let catResponse = await Promise.all(fetchCatsArr);
+
+    const catDataArray = await Promise.all(
+      catResponse.map(async (response) => {
+        return await response.json();
+      })
+    );
+    dispatch(addAllCats(catDataArray));
+    console.log(catDataArray);
+  };
+  useEffect(() => {
+    fetchAllCats();
+  }, []);
+};
+
+export default useMovieCategories;
