036c6ab05e6867ff2e374c0da45f16b2c779a096
ltr <apuroopgupca007@gmail.com>
2023-10-02 12:03:24 +0530

    Updated Secondary container and video bg

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((store) => store.movies);
+  return (
+    movies?.nowPlayingMovies && (
+      <div className=" bg-black">
+        <div className="-mt-52 relative z-30">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          {/* make it dynamic */}
+        </div>
+      </div>
+    )
+  );
 };
 
 export default SecondaryContainer;
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -8,8 +8,9 @@ const VideoBackground = ({ movieId }) => {
     <div className="w-screen">
       <iframe
         className="w-screen aspect-video    "
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1`}
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1&controls=0`}
         title="YouTube video player"
+        allowFullScreen
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </div>
